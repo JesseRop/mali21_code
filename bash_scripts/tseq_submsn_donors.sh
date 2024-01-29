976b3a7b8d8e1ed312621e728d31b697c34b0bfc
Jesse Rop <rop.jesse@gmail.com>
2024-01-29 14:39:23 +0000

    submission of tradeseq jobs

--- bash_scripts/tseq_submsn_donors.sh
+++ bash_scripts/tseq_submsn_donors.sh
@@ -5,11 +5,13 @@
 module load ISG/R/4.1.0
 
 ## Job submission variables
-mem='-R "span[hosts=1] select[mem>160000] rusage[mem=160000]" -M 160000'
-ncores=14
+mem='-R "span[hosts=1] select[mem>60000] rusage[mem=60000]" -M 60000'
+ncores=10
 
 ##job variables
-list_item=($(seq 1 1 28))
+# list_item=($(seq 1 1 28))
+list_item=($(seq 17 1 28))
+# list_item=(15 16 23 24 27 7 8)
 # list_item=($(seq 3 1 8))
 # list_item=(3 4 5 6 7 8 9 10 11 12 13 14)
 # list_item=(1 2)
